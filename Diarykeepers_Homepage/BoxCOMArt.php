e5e312265c4561b48e20d3499da39751cc273143
Alcea <17431197+Ry3yr@users.noreply.github.com>
2023-03-27 21:17:56 +0200

    Add files via upload

--- Diarykeepers_Homepage/BoxCOMArt.php
+++ Diarykeepers_Homepage/BoxCOMArt.php
@@ -1,6 +1,40 @@
 <?php
+unlink("yourpage.html");
+ob_start();
 $dom = new DOMDocument;
+@$dom->loadHTML($html);
+$links = $dom->getElementsByTagName('a');
+$url = 'https://m.box.com/shared_item/https%3A%2F%2Fapp.box.com%2Fs%2Fj7i88s6jb4yyk4wmiti4tol8ejoikdhl?page=9';
+$html = file_get_contents($url);
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
+$xpath = new DOMXPath($dom);
+$nodes = $xpath->query('//a[@class="files-item-anchor"]');
+foreach ($nodes as $node){
+    //echo $link->nodeValue;
+    //echo "<a class href=https://m.box.com";
+    echo $node-> getAttribute('href');
+    //echo $node-> getAttribute('href'), ">";
+    //echo $node-> getAttribute('href'), "</a>", '<br>';
+    echo "/download?shared_link=https%3A%2F%2Fapp.box.com%2Fs%2Fj7i88s6jb4yyk4wmiti4tol8ejoikdhl";
+    echo ">",'</img>',"\n";
+}
+?>
 
+<?php
+//echo '1';
+file_put_contents('yourpage.html', ob_get_contents());
+?>
+<?php
+$path_to_file = 'yourpage.html';
+$file_contents = file_get_contents($path_to_file);
+$file_contents = str_replace("/shared_item/https%3A%2F%2Fapp.box.com%2Fs%2Fj7i88s6jb4yyk4wmiti4tol8ejoikdhl/view/", "<img src=https://m.box.com/file/", $file_contents);
+file_put_contents($path_to_file, $file_contents);
+?>
+
+
+<?php
+$dom = new DOMDocument;
 @$dom->loadHTML($html);
 $links = $dom->getElementsByTagName('a');
 $url = 'https://m.box.com/shared_item/https%3A%2F%2Fapp.box.com%2Fs%2Fj7i88s6jb4yyk4wmiti4tol8ejoikdhl?page=9';
@@ -12,11 +46,19 @@ $nodes = $xpath->query('//a[@class="files-item-anchor"]');
 
 foreach ($nodes as $node){
     echo $link->nodeValue;
-    echo "<a target='_blank' href=";
-    echo "https://m.box.com";
-    echo $node-> getAttribute('href');
-    echo ">";
-    echo $node-> getAttribute('href');
-    echo "</a>", '<br>';
+    echo "<a target='_blank' href=https://m.box.com";
+    echo $node-> getAttribute('href'), ">";
+
+    echo $node-> getAttribute('href'), "</a>", '<br>';
 }
 ?>
+
+
+
+<?php echo '<script type="text/javascript"> window.open("yourpage.html"); </script>'; ?>
+
+
+
+
+
+
