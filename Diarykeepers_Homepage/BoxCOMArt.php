f1b1021ec3dbb052747180c09e5dfba1e864a112
Alcea <17431197+Ry3yr@users.noreply.github.com>
2023-03-31 17:38:42 +0200

    Add files via upload

--- Diarykeepers_Homepage/BoxCOMArt.php
+++ Diarykeepers_Homepage/BoxCOMArt.php
@@ -34,25 +34,18 @@ foreach ($nodes as $node){
     echo "/download?shared_link=https%3A%2F%2Fapp.box.com%2Fs%2Fj7i88s6jb4yyk4wmiti4tol8ejoikdhl";
     echo ">",'</img>',"\n";
 }
-?>
 
-<?php
 //echo '1';
 file_put_contents('yourpage.html', ob_get_contents());
-?>
-<?php
+
 $path_to_file = 'yourpage.html';
 $file_contents = file_get_contents($path_to_file);
 $file_contents = str_replace("/shared_item/https%3A%2F%2Fapp.box.com%2Fs%2Fj7i88s6jb4yyk4wmiti4tol8ejoikdhl/view/", "<img src=https://m.box.com/file/", $file_contents);
 file_put_contents($path_to_file, $file_contents);
-?>
 
-
-<?php
 $dom = new DOMDocument;
 @$dom->loadHTML($html);
 $links = $dom->getElementsByTagName('a');
-$url = 'https://m.box.com/shared_item/https%3A%2F%2Fapp.box.com%2Fs%2Fj7i88s6jb4yyk4wmiti4tol8ejoikdhl?page=9';
 $html = file_get_contents($url);
 $dom = new DOMDocument();
 @$dom->loadHTML($html);
